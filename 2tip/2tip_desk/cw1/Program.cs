d9ca4e7042515fc436e35de77d7e8def53ce28c6
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-09-03 16:48:11 +0200

    kk

--- 2tip/2tip_desk/cw1/Program.cs
+++ 2tip/2tip_desk/cw1/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("***********************");
+Console.WriteLine("*     Adam Nowak      *");
+Console.WriteLine("*        2TIP         *");
+Console.WriteLine("***********************");
+    
+// w c# zmienne definiujemy 
+// typ zmiennej nazwa_zmiennej = wartość;
+//liczba całkowita --> int
+int liczba = 5;
+liczba = 67;
+//liczba = "67"; //błąd kompilacji
+//łancuch znaków --> string
+string tekst = "Hello, World!";
+
+//zmiana wartości zmiennej tekst
+tekst = "Hello, C#!";
+//tekst = 56;
+Console.WriteLine(liczba); //użycie zmiennej liczba
+Console.WriteLine(tekst); //użycie zmiennej tekst
