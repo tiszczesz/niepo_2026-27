2cf1a77c96726115fb5848d91083cc371716e94f
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-09-17 11:35:09 +0200

    kk

---
+++ 2tip/2tip_desk/cw3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Console.WriteLine("Hello, World!");// "\n"
+// Console.Write("Hello, World!");
+Console.Write("Podaj swoje imię: ");
+string? firstname = Console.ReadLine();
+Console.Write("Podaj swoje nazwisko: ");
+string? lastname = Console.ReadLine();
+Console.WriteLine($"Witaj {firstname} {lastname}!");
+
+//definicja funkcji
+//typ_zwracany(void) nazwa_funkcji(parametry)
+// {     ciało funkcji}
+
+//funkcja majaca dwa argumenty typu string i zwracająca string
+string GetFullName(string fname, string lname){ //nagłówek funkcji
+    //ciało funkcji
+    return $"{fname} {lname}";
+}
+string result = GetFullName(firstname, lastname);
+Console.WriteLine($"To jest z funkcji: {result}!");
+
+//funkcja z dwoma argumentami typu int i nic nie zwracająca (void)
+void ShowSum(int a, int b){
+    int sum = a + b;
+    Console.WriteLine($"Suma {a} i {b} wynosi: {sum}");    
+}
+
+//wywołanie funkcji ShowSum
+ShowSum(5, 10);
+ShowSum(24, 1);
