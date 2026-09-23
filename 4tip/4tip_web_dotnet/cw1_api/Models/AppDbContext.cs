112db4ced39ea44d4fd50e96a83258c16960f8f4
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-09-23 15:53:25 +0200

    kk

--- 4tip/4tip_web_dotnet/cw1_api/Models/AppDbContext.cs
+++ 4tip/4tip_web_dotnet/cw1_api/Models/AppDbContext.cs
@@ -18,16 +18,16 @@ public class AppDbContext : DbContext
         base.OnModelCreating(modelBuilder);
         //seeding danych do tabeli Books
         modelBuilder.Entity<Book>().HasData(
-            new Book { Id = 1, Title = "Lalka", Author = "Bolesław Prus" },
-            new Book { Id = 2, Title = "Zbrodnia i kara", Author = "Fiodor Dostojewski" },
-            new Book { Id = 3, Title = "Duma i uprzedzenie", Author = "Jane Austen" },
-            new Book { Id = 4, Title = "Władca Pierścieni", Author = "J.R.R. Tolkien" },
-            new Book { Id = 5, Title = "Mały Książę", Author = "Antoine de Saint-Exupéry" },
-            new Book { Id = 6, Title = "1984", Author = "George Orwell" },
-            new Book { Id = 7, Title = "Mistrz i Małgorzata", Author = "Michaił Bułhakow" },
-            new Book { Id = 8, Title = "Sto lat samotności", Author = "Gabriel García Márquez" },
-            new Book { Id = 9, Title = "Hobbit", Author = "J.R.R. Tolkien" },
-            new Book { Id = 10, Title = "Ostatnie życzenie", Author = "Andrzej Sapkowski" }
+            new Book { Id = 1, Title = "Lalka", Author = "Bolesław Prus", ReleaseDate = new DateTime(1890, 1, 1) },
+            new Book { Id = 2, Title = "Zbrodnia i kara", Author = "Fiodor Dostojewski", ReleaseDate = new DateTime(1866, 1, 1) },
+            new Book { Id = 3, Title = "Duma i uprzedzenie", Author = "Jane Austen", ReleaseDate = new DateTime(1813, 1, 1) },
+            new Book { Id = 4, Title = "Władca Pierścieni", Author = "J.R.R. Tolkien", ReleaseDate = new DateTime(1954, 1, 1) },
+            new Book { Id = 5, Title = "Mały Książę", Author = "Antoine de Saint-Exupéry", ReleaseDate = new DateTime(1943, 1, 1) },
+            new Book { Id = 6, Title = "1984", Author = "George Orwell", ReleaseDate = new DateTime(1949, 1, 1) },
+            new Book { Id = 7, Title = "Mistrz i Małgorzata", Author = "Michaił Bułhakow", ReleaseDate = new DateTime(1967, 1, 1) },
+            new Book { Id = 8, Title = "Sto lat samotności", Author = "Gabriel García Márquez", ReleaseDate = new DateTime(1967, 1, 1) },
+            new Book { Id = 9, Title = "Hobbit", Author = "J.R.R. Tolkien", ReleaseDate = new DateTime(1937, 1, 1) },
+            new Book { Id = 10, Title = "Ostatnie życzenie", Author = "Andrzej Sapkowski", ReleaseDate = new DateTime(1993, 1, 1) }
         );
     }
 }
