3396300598f4841176525e40da73c47e5bde5edb
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-09-23 15:51:05 +0200

    kk

--- 4tip/4tip_web_dotnet/cw1_api/Models/AppDbContext.cs
+++ 4tip/4tip_web_dotnet/cw1_api/Models/AppDbContext.cs
@@ -14,6 +14,20 @@ public class AppDbContext : DbContext
     //dodanie danych do bazy podczas tworzenia bazy
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        //wywołanie metody bazowej
         base.OnModelCreating(modelBuilder);
+        //seeding danych do tabeli Books
+        modelBuilder.Entity<Book>().HasData(
+            new Book { Id = 1, Title = "Lalka", Author = "Bolesław Prus" },
+            new Book { Id = 2, Title = "Zbrodnia i kara", Author = "Fiodor Dostojewski" },
+            new Book { Id = 3, Title = "Duma i uprzedzenie", Author = "Jane Austen" },
+            new Book { Id = 4, Title = "Władca Pierścieni", Author = "J.R.R. Tolkien" },
+            new Book { Id = 5, Title = "Mały Książę", Author = "Antoine de Saint-Exupéry" },
+            new Book { Id = 6, Title = "1984", Author = "George Orwell" },
+            new Book { Id = 7, Title = "Mistrz i Małgorzata", Author = "Michaił Bułhakow" },
+            new Book { Id = 8, Title = "Sto lat samotności", Author = "Gabriel García Márquez" },
+            new Book { Id = 9, Title = "Hobbit", Author = "J.R.R. Tolkien" },
+            new Book { Id = 10, Title = "Ostatnie życzenie", Author = "Andrzej Sapkowski" }
+        );
     }
 }
