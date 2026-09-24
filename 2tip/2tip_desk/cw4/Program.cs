7a9e37a1ebea11aec7ffdf6651bdb16659e72710
tiszczesz <p.szczeszek@lo-niepolomice.pl>
2026-09-24 11:51:55 +0200

    kk

---
+++ 2tip/2tip_desk/cw4/Program.cs
@@ -0,0 +1,16 @@
+﻿void ex1()
+{
+    //petla while(war) {...}
+    Random rnd = new Random();// losowacz liczb
+    const uint range = 100; //liczby całkowite dodatnie
+    uint sum = 0;
+    while(sum < range)
+    {
+        //losujemy i rzutujemy na dodatnie
+        uint losowa = (uint)rnd.Next(0,20); 
+        sum += losowa; //sum = sum + losowa
+        Console.Write(losowa+" ");
+    }
+    Console.WriteLine("\n Suma koncowa = "+ sum);
+}
+ex1();
